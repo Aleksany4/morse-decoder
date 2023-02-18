7ab2a31533bd66f10f59460e8b0284c35f0abc8a
Aleksany4 <aleksany4pl@gmail.com>
2023-02-18 17:12:25 +0100

    init: complete

--- src/index.js
+++ src/index.js
@@ -35,11 +35,39 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '     ':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+  let arr = [], b = 0, x = 0;
+    for (let i = 0; i < expr.length; i += 2) {
+        arr[x] = expr.slice(b, b += 2); {
+            x++
+        };
+    };
+      let arrObj = [];
+    for (j = 0; j < arr.length; j++) {
+    if (arr[j].includes("11")) {
+      arrObj.push('-')
+    } else if (arr[j].includes("10")) {
+      arrObj.push('.')
+    } else if (arr[j].includes('**')) {
+      arrObj.push(' ')
+    } else if (arr[j].includes("00")) {
+      arrObj.push('')
+    }
+    }
+  let subArr = [], size = 5;
+  for (let i = 0; i < arrObj.length/size; i++){
+    subArr[i] = arrObj.slice((i*size), (i*size) + size).join('');
+    }
+  let goal = '';
+  for (let t = 0; t < subArr.length; t++) {
+    goal = goal + MORSE_TABLE[subArr[t]]
+    console.log(MORSE_TABLE[subArr[t]])
+  }
+  return goal;
+};
 
 module.exports = {
     decode
